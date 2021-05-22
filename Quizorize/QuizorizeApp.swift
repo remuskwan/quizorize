348e902fb919bb94c35193d8af303acc98d2a39e
cjunhong99 <77767883+cjunhong99@users.noreply.github.com>
2021-05-22 22:13:35 +0800

    Update QuizorizeApp.swift

--- Quizorize/QuizorizeApp.swift
+++ Quizorize/QuizorizeApp.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 import Firebase
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
 
 @main
 struct QuizorizeApp: App {
