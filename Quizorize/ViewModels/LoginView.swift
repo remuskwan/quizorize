02c82a248cef7e578d8cdb8d9baa40b872fe9f52
cjunhong99 <77767883+cjunhong99@users.noreply.github.com>
2021-05-22 22:11:25 +0800

    Update LoginView.swift

--- Quizorize/ViewModels/LoginView.swift
+++ Quizorize/ViewModels/LoginView.swift
@@ -22,7 +22,70 @@ struct LoginView_Previews: PreviewProvider {
 }
 
 struct Login : View {
+    @State var email: String = ""
+    @State var password: String = ""
     
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image("flashcard")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("Study Hard, Study Smart.")
+                    .font(.title3)
+                
+                HStack {
+                    Image("apple")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .center)
+                    Image("google")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .center)
+                    Image("microsoft")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100, alignment: .center)
+                }
+
+                VStack {
+                    TextField("Email Address", text: $email)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Sign In")
+                            .cornerRadius(8)
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 50)
+                            .background(Color.purple)
+                    })
+                    .padding(4)
+                    
+                    NavigationLink(
+                        destination: RegisterView(),
+                        label: {
+                            Text("Register For An Account")
+                                .cornerRadius(8)
+                                .foregroundColor(.purple)
+                                .frame(width: 200, height: 50)
+                                .background(Color.white)
+                                .border(Color.gray)
+                        })
+                }
+                .padding()
+            }
+                Spacer()
+        }
+    }
+    
+    /*
     @State var color = Color.black.opacity(0.7)
     @State var email = ""
     @State var password = ""
@@ -51,4 +114,5 @@ struct Login : View {
 //            })
         }
     }
+ */
 }
